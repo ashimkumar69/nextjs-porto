66b1915b8c41d9276fddba6702b57cf267d4949f
ASHIM KUMAR SHAHA <ashimkumar69@gmail.com>
2021-06-27 09:27:34 +0600

    menu dropdown design going

--- components/navbar/appBar.js
+++ components/navbar/appBar.js
@@ -32,7 +32,7 @@ import Grid from "@material-ui/core/Grid";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Fade from "@material-ui/core/Fade";
-
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 // import
 import clsx from "clsx";
 import { useLoaded } from "../../hooks/loaded";
@@ -41,26 +41,100 @@ import Scrollspy from "react-scrollspy";
 import { useRef } from "react";
 
 const menuLists = [
-  { id: 1, title: "Home", path: "home" },
-  { id: 2, title: "About", path: "about" },
-  { id: 3, title: "Services", path: "services" },
-  { id: 4, title: "Projects", path: "projects" },
-  { id: 5, title: "Testimonials", path: "testimonials" },
-  { id: 6, title: "News", path: "news" },
-  { id: 7, title: "Contact", path: "contact" },
-  { id: 8, title: "Pages", path: "", dropdown: true },
-];
+  { id: 1, title: "Home", path: "#home", submenu: [] },
+  { id: 2, title: "About", path: "#about", submenu: [] },
+  { id: 3, title: "Services", path: "#services", submenu: [] },
+  { id: 4, title: "Projects", path: "#projects", submenu: [] },
+  { id: 5, title: "Testimonials", path: "#testimonials", submenu: [] },
+  { id: 6, title: "News", path: "#news", submenu: [] },
+  { id: 7, title: "Contact", path: "#contact", submenu: [] },
+  {
+    id: 8,
+    title: "Pages 1",
+    path: "",
+    submenu: [
+      {
+        id: 1,
+        title: "Auth",
+        path: "",
+        submenu: [
+          {
+            id: 1,
+            title: "Login",
+            path: "/login",
+            submenu: [],
+          },
 
-const NavLinkItemButton = withStyles((theme) => ({
-  root: {
-    padding: "20px 15px",
-    borderRadius: 0,
-    borderBottom: "2px solid transparent",
-    "&:hover": {
-      borderBottom: `2px solid ${theme.palette.common.white}`,
-    },
+          { id: 2, title: "Register", path: "/register", submenu: [] },
+          {
+            id: 3,
+            title: "Reset Password",
+            path: "/reset-password",
+            submenu: [],
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Error Page",
+        path: "",
+        submenu: [
+          { id: 1, title: "404 Page", path: "", submenu: [] },
+          { id: 2, title: "500 Page", path: "", submenu: [] },
+        ],
+      },
+      {
+        id: 3,
+        title: "Settings",
+        path: "/login",
+        submenu: [],
+      },
+    ],
   },
-}))(Button);
+  {
+    id: 9,
+    title: "Pages 2",
+    path: "",
+    submenu: [
+      {
+        id: 1,
+        title: "index 1",
+        path: "",
+        submenu: [
+          {
+            id: 1,
+            title: "Login",
+            path: "/login",
+            submenu: [],
+          },
+
+          { id: 2, title: "Register", path: "/register", submenu: [] },
+          {
+            id: 3,
+            title: "Reset Password",
+            path: "/reset-password",
+            submenu: [],
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "index 2",
+        path: "",
+        submenu: [
+          { id: 1, title: "404 Page", path: "", submenu: [] },
+          { id: 2, title: "500 Page", path: "", submenu: [] },
+        ],
+      },
+      {
+        id: 3,
+        title: "index 3",
+        path: "",
+        submenu: [],
+      },
+    ],
+  },
+];
 
 const LogoButton = withStyles(() => ({
   root: {
@@ -74,6 +148,14 @@ const LogoButton = withStyles(() => ({
 }))(Button);
 
 const useStyles = makeStyles((theme) => ({
+  navLinkButton: {
+    padding: "20px 15px",
+    borderRadius: 0,
+    borderBottom: "2px solid transparent",
+    "&:hover": {
+      borderBottom: `2px solid ${theme.palette.common.white}`,
+    },
+  },
   navList: {
     display: "flex",
     listStyle: "none",
@@ -94,7 +176,7 @@ function Nav(props) {
   const classes = useStyles();
   // nextjs
   const router = useRouter();
-  
+
   // load component cliend site
   const loaded = useLoaded();
   // theme color
@@ -103,24 +185,62 @@ function Nav(props) {
     isThemeLight ? dispatch(themeDarkAction()) : dispatch(themeLightAction());
   };
 
-  // dropdown menu
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const openDropdown = Boolean(anchorEl);
-  const handleOpenDropdownClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  // dropdown menu Level1
+  const [anchorElLevel1, setAnchorElLevel1] = React.useState(null);
+  const [selectedIndexLevel1, setSelectedIndexLevel1] = React.useState(null);
+  const openDropdownLevel1 = Boolean(anchorElLevel1);
+  const handleOpenDropdownClickLevel1 = (event, index, submenu, path) => {
+    setSelectedIndexLevel1(index);
+    submenu.length > 0 ? setAnchorElLevel1(event.currentTarget) : "";
+    if (path.trim() !== "" && path.trim().length !== 0) {
+      handleNavLinkOnClick(event, path);
+    }
   };
-  const handleCloseDropdownClick = () => {
-    setAnchorEl(null);
+  const handleCloseDropdownClickLevel1 = () => {
+    setAnchorElLevel1(null);
   };
+
+  // dropdown menu Level2
+  const [anchorElLevel2, setAnchorElLevel2] = React.useState(null);
+  const [selectedIndexLevel2, setSelectedIndexLevel2] = React.useState(null);
+  const openDropdownLevel2 = Boolean(anchorElLevel2);
+  const handleOpenDropdownClickLevel2 = (event, index, submenu, path) => {
+    setSelectedIndexLevel2(index);
+    submenu.length > 0 ? setAnchorElLevel2(event.currentTarget) : "";
+    if (path.trim() !== "" && path.trim().length !== 0) {
+      handleNavLinkOnClick(event, path);
+    }
+  };
+  const handleCloseDropdownClickLevel2 = () => {
+    setAnchorElLevel2(null);
+  };
+
+  // dropdown menu Level3
+  // const [anchorElLevel3, setAnchorElLevel3] = React.useState(null);
+  // const [selectedIndexLevel3, setSelectedIndexLevel3] = React.useState(null);
+  // const openDropdownLevel3 = Boolean(anchorElLevel3);
+  // const handleOpenDropdownClickLevel3 = (event, index, submenu, path) => {
+  //   setSelectedIndexLevel3(index);
+  //   submenu.length > 0 ? setAnchorElLevel3(event.currentTarget) : "";
+  //   if (path.trim() !== "" && path.trim().length !== 0) {
+  //     handleNavLinkOnClick(event, path);
+  //   }
+  // };
+  // const handleCloseDropdownClickLevel3 = () => {
+  //   setAnchorElLevel3(null);
+  // };
 
   // manage navlink onclick
-  const handleNavLinkOnClick = ({ event, path }) => {
-    const anchor = (event.target.ownerDocument || document).querySelector(
-      `#${path}`
-    );
-
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: "smooth", block: "start" });
+  const handleNavLinkOnClick = (event, path) => {
+    if (path.includes("#")) {
+      const anchor = (event.target.ownerDocument || document).querySelector(
+        `${path}`
+      );
+      if (anchor) {
+        anchor.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    } else {
+      router.push(path);
     }
   };
 
@@ -167,52 +287,122 @@ function Nav(props) {
                     className={classes.navList}
                     offset={-10}
                   >
-                    {menuLists.map((menuList) => (
-                      <Box component="li" key={menuList.id}>
-                        <NavLinkItemButton
+                    {menuLists.map((item, index) => (
+                      <Box component="li" key={item.id}>
+                        <Button
+                          className={clsx(classes.navLinkButton)}
                           component="a"
                           color="inherit"
                           onClick={(event) =>
-                            menuList.dropdown
-                              ? handleOpenDropdownClick(event)
-                              : handleNavLinkOnClick({
-                                  event,
-                                  path: menuList.path,
-                                })
+                            handleOpenDropdownClickLevel1(
+                              event,
+                              index,
+                              item.submenu,
+                              item.path
+                            )
                           }
-                          aria-controls={menuList.dropdown && "fade-menu"}
-                          aria-haspopup={menuList.dropdown && "true"}
-                          className={clsx(classes.navLinkItem)}
+                          aria-controls={
+                            item.submenu.length > 0 ? "fade-menu" : ""
+                          }
+                          aria-haspopup={
+                            item.submenu.length > 0 ? "true" : "false"
+                          }
                         >
-                          {menuList.title}
+                          {item.title}
 
-                          {menuList.dropdown &&
-                            (openDropdown ? <ExpandLess /> : <ExpandMore />)}
-                        </NavLinkItemButton>
+                          {item.submenu.length > 0 ? (
+                            index === selectedIndexLevel1 &&
+                            openDropdownLevel1 ? (
+                              <ExpandLess />
+                            ) : (
+                              <ExpandMore />
+                            )
+                          ) : (
+                            <React.Fragment>{""}</React.Fragment>
+                          )}
+                        </Button>
+                        {item.submenu.length > 0 &&
+                          index === selectedIndexLevel1 && (
+                            <React.Fragment>
+                              <Menu
+                                id="fade-menu"
+                                anchorEl={anchorElLevel1}
+                                keepMounted
+                                open={openDropdownLevel1}
+                                onClose={handleCloseDropdownClickLevel1}
+                                TransitionComponent={Fade}
+                              >
+                                {item.submenu.map((item, index) => (
+                                  <MenuItem key={item.id}>
+                                    <Button
+                                      fullWidth
+                                      component="a"
+                                      color="inherit"
+                                      onClick={(event) =>
+                                        handleOpenDropdownClickLevel2(
+                                          event,
+                                          index,
+                                          item.submenu,
+                                          item.path
+                                        )
+                                      }
+                                      aria-controls={
+                                        item.submenu.length > 0
+                                          ? "fade-menu-2"
+                                          : ""
+                                      }
+                                      aria-haspopup={
+                                        item.submenu.length > 0
+                                          ? "true"
+                                          : "false"
+                                      }
+                                    >
+                                      {item.title}
+
+                                      {item.submenu.length > 0 ? (
+                                        index === selectedIndexLevel2 &&
+                                        openDropdownLevel2 ? (
+                                          <ExpandLess />
+                                        ) : (
+                                          <ExpandMore />
+                                        )
+                                      ) : (
+                                        <React.Fragment>{""}</React.Fragment>
+                                      )}
+                                    </Button>
+                                    {item.submenu.length > 0 &&
+                                      index === selectedIndexLevel2 && (
+                                        <React.Fragment>
+                                          <Menu
+                                            id="fade-menu-2"
+                                            anchorEl={anchorElLevel2}
+                                            keepMounted
+                                            open={openDropdownLevel2}
+                                            onClose={
+                                              handleCloseDropdownClickLevel2
+                                            }
+                                            TransitionComponent={Fade}
+                                          >
+                                            {item.submenu.map((item, index) => (
+                                              <MenuItem
+                                                key={item.id}
+                                                onClick={
+                                                  handleCloseDropdownClickLevel2
+                                                }
+                                              >
+                                                {item.title}
+                                              </MenuItem>
+                                            ))}
+                                          </Menu>
+                                        </React.Fragment>
+                                      )}
+                                  </MenuItem>
+                                ))}
+                              </Menu>
+                            </React.Fragment>
+                          )}
                       </Box>
                     ))}
-
-                    <Menu
-                      id="fade-menu"
-                      anchorEl={anchorEl}
-                      keepMounted
-                      open={openDropdown}
-                      onClose={handleCloseDropdownClick}
-                      TransitionComponent={Fade}
-                    >
-                      <MenuItem onClick={handleCloseDropdownClick}>
-                        Login
-                      </MenuItem>
-                      <MenuItem onClick={handleCloseDropdownClick}>
-                        Register
-                      </MenuItem>
-                      <MenuItem onClick={handleCloseDropdownClick}>
-                        Reset Password
-                      </MenuItem>
-                      <MenuItem onClick={handleCloseDropdownClick}>
-                        404 Error Page
-                      </MenuItem>
-                    </Menu>
                   </Scrollspy>
                 </Box>
               </Grid>
@@ -239,8 +429,6 @@ function Nav(props) {
           </Toolbar>
         </Container>
       </AppBar>
-
-     
     </Fragment>
   );
 }
